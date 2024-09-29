6a9ee50e9f5ec99c7f95a67684469f05086a4b84
Alexandre Queiroz <xandeq@gmail.com>
2024-09-29 11:43:20 -0300

    Default commit message

--- controllers/goalsController.js
+++ controllers/goalsController.js
@@ -1,4 +1,4 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 const db = admin.firestore();
 
 // Função para adicionar metas no Firebase
@@ -48,24 +48,21 @@ exports.addGoal = async (req, res) => {
 exports.deleteGoal = async (req, res) => {
   try {
     const { goalId } = req.params;
+    console.log('Goal ID:', goalId); // Log para verificar o goalId
 
     if (!goalId) {
-      return res.status(400).json({ message: "Goal ID is required" });
+      return res.status(400).json({ message: 'Goal ID is required' });
     }
 
     const goalRef = db.collection('goals').doc(goalId);
-    
-    // Verifica se a meta existe
+
     const doc = await goalRef.get();
     if (!doc.exists) {
-      return res.status(404).json({ message: "Goal not found" });
+      return res.status(404).json({ message: 'Goal not found' });
     }
 
-    // Deleta a meta
     await goalRef.delete();
-    console.log(`Goal with ID ${goalId} deleted successfully`);
-    
-    res.status(200).json({ message: "Goal deleted successfully" });
+    res.status(200).json({ message: 'Goal deleted successfully' });
   } catch (error) {
     console.error('Erro ao deletar a meta:', {
       message: error.message,
@@ -89,17 +86,16 @@ exports.deleteGoal = async (req, res) => {
   }
 };
 
-
 // Função para obter metas do Firebase
 exports.getGoals = async (req, res) => {
   try {
     const { userId } = req.query;
 
     if (!userId) {
-      return res.status(400).json({ message: "User ID is required" });
+      return res.status(400).json({ message: 'User ID is required' });
     }
 
-    const goalsSnapshot = await db.collection("goals").where("userId", "==", userId).get();
+    const goalsSnapshot = await db.collection('goals').where('userId', '==', userId).get();
 
     const goals = goalsSnapshot.docs.map((doc) => ({
       id: doc.id,
