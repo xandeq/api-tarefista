4b39be85ec85fab98f0a7499fa400704b36d51fd
Alexandre Queiroz <xandeq@gmail.com>
2024-09-29 11:48:57 -0300

    Default commit message

--- routes/goals.js
+++ routes/goals.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addGoal, getGoals } = require('../controllers/goalsController');
+const { addGoal, getGoals, deleteGoal } = require('../controllers/goalsController');
 
 // Rota para adicionar metas
 router.post('/goals', addGoal);
-
 // Rota para obter metas
 router.get('/goals', getGoals);
 router.delete('/goals/:goalId', deleteGoal);
